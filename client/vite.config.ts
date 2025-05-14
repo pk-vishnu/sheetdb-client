530e2e7544ac560d422ed4943f038f11c160db14
Vishnu P K <vishnuvpk05@gmail.com>
2025-05-14 16:50:58 +0530

    feat: manifest json, progressive web app config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,33 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
+      VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt'],
+      manifest: {
+        name: 'SheetDB PWA',
+        short_name: 'SDB PWA',
+        description: 'A Progressive Web App for SheetDB Client',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
 })
